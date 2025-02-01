0578c6498f42d8d97d758ea9cf71bb8f1e267157
Abdul Baari Davids <abdulbaaridavids04@gmail.com>
2025-02-01 22:17:30 +0200

    Initial Commit

---
+++ HNMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HNMenu
+//
+//  Created by Abdul Baari Davids on 2025/02/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HNMenu/HNMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  HNMenuApp.swift
+//  HNMenu
+//
+//  Created by Abdul Baari Davids on 2025/02/01.
+//
+
+import SwiftUI
+
+@main
+struct HNMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
